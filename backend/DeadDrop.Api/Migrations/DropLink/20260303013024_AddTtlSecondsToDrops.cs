e701814481ea16cd6d9843d5eb3b982056c26519
Cydo Entis <cydoentis@gmail.com>
2026-03-02 22:03:03 -0500

    chore: add migration for TtlSeconds column and nullable ExpiresAt

---
+++ backend/DeadDrop.Api/Migrations/DropLink/20260303013024_AddTtlSecondsToDrops.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeadDrop.Migrations.DropLink
+{
+    /// <inheritdoc />
+    public partial class AddTtlSecondsToDrops : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiresAt",
+                table: "Drops",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TtlSeconds",
+                table: "Drops",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TtlSeconds",
+                table: "Drops");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiresAt",
+                table: "Drops",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
